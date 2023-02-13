ed8437b02b6b6c36d39654d8d3c6dfe91d29b29b
suh_jm <suhjm5009@dencomm.ai>
2023-02-13 10:46:47 +0900

    Webpack & Babel 환경설정

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+        <h1>Webpack Open</h1>
+    </div>
+  )
+};
+ReactDOM.render(<App />, document.getElementById('app'));
+export default App;
