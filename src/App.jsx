d99ef4834253378fa7b8228229cc8fbc1cceb975
Nahyeon Kim <nahyeonkim@d-ncomm.ai>
2023-02-15 15:26:49 +0900

    React 프로젝트 환경설정 - 2.Typescript 설정

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <h1>Hello World!</h1>;
-};
-
-// index.html의 'app'이라는 id를 가진 div에 리액트 코드를 삽입해준다
-ReactDOM.render(<App />, document.getElementById('app')); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,40 @@
-// react_start/webpack.config.js
-const path = require('path'); // 절대 경로를 참조하기 위한 코드입니다.
+const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // webpack에서 htmml을 다루기 위한 플러그인입니다.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin'); // TS의 type 검사를 별도로 실행하는 웹팩 플러그인입니다.
 
 module.exports = {
-  entry: {
-    'js/app': ['./src/App.jsx'],  // 최종적으로 하나로 합쳐질(번들링) 파일 경로입니다. (최상위 컴포넌트의 경로를 적어주면 됩니다.)
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/'), // 생성된 번들(bundle) 파일이 저장되는 위치입니다.
-    publicPath: '/', // 달른 파일에서 생성된 번들을 참조할 때 '/'를 기준으로 참조합니다.
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/, // 리액트 파일인 jsx와 js는 babel을 이용하여 빌드합니다.
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
+    entry: {
+        'js/app': ['./src/App.tsx'],  // **For TS - App.tsx로 변경
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/, // **For TS - 확장자 tsx로 변경
+                use: [
+                'babel-loader',
+                {
+                    loader: 'ts-loader', // **For TS - webpack에서 TS를 사용하기 위해 추가하였습니다.
+                    options: { 
+                        // **For TS - 타입체크는 fork-ts-checker-webpack-plugin이 수행
+                        transpileOnly: true,
+                    },
+                },
+                ],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new ForkTsCheckerWebpackPlugin({typescript: { silent: true }}), // **For TS - TS의 컴파일 속도 향상을 위한 플러그인을 설정
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',  // template: 기본 생성된 HTML 대신 사용자 본인의 템플릿 파일을 전달합니다.
-      filename: 'index.html',
-    }),
-  ],
 };
